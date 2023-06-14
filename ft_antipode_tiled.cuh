91fe22de1cb9704573905961ee476aa41fad85de
inakleinbottle <admin@inakleinbottle.com>
2023-06-14 07:58:40 +0100

    filled out tiled antipode implementation

--- ft_antipode_tiled.cuh
+++ ft_antipode_tiled.cuh
@@ -36,12 +36,69 @@
 #include "common.cuh"
 
 
+
+struct AntipodeInfo {
+    int32_t width;
+    int32_t depth;
+    int32_t tile_letters;
+    int32_t untiled_size;
+    const int32_t* levels;
+};
+
+
 template <typename S>
 __global__ void ft_antipode_high_kernel(
     rp_t<S> pd_out,
     crp_t<S> pd_in,
-    int32_t degree
+    AntipodeInfo info
     ) {
+    const auto& width = info.width;
+    const auto& ix = threadIdx.x;
+    const auto& iy = threadIdx.y;
+    const auto untiled_idx = ix*blockDim.y + iy;
+    const auto& tile_width = info.levels[info.tile_letters];
+
+    const auto tot_tile_let = 2 * info.tile_letters;
+
+    if (untiled_idx < info.untiled_size) {
+        int32_t degree;
+        const auto reverse_idx = reverse_idx_to(untiled_idx, width, &degree);
+        if ((degree & 1) == 0) {
+            pd_out[reverse_idx] = pd_in[untiled_idx];
+        } else {
+            pd_out[reverse_idx] = -pd_in[untiled_idx];
+        }
+    }
+
+    const auto rix = reverse_idx(ix, width, info.tile_letters);
+    const auto riy = reverse_idx(iy, width, info.tile_letters);
+
+
+
+    for (int32_t out_deg=tot_tile_let; out_deg < info.depth; ++out_deg) {
+        const auto mid_deg = out_deg - tot_tile_let;
+        const auto& mid_stride = info.levels[mid_deg];
+
+        for (int32_t idx=blockIdx.x; idx < info.levels[mid_deg]; idx += blockDim.x) {
+            const auto ridx = reverse_idx(idx, width, mid_deg);
+            if (ix < tile_width && iy < tile_width) {
+                const auto read = (ix*mid_stride + idx)*tile_width + iy;
+                const auto write = (riy*mid_stride + ridx)*tile_width + rix;
+
+
+                if ((mid_deg & 1) == 0) {
+                    pd_out[write] = pd_in[read];
+                } else {
+                    pd_out[write] = -pd_in[read];
+                }
+            }
+
+        }
+
+    }
+
+
+
 
 }
 
