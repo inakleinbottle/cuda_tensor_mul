bf9f3f02e3e2d03cdfc032ce4dad431bcc00a1ca
inakleinbottle <admin@inakleinbottle.com>
2023-06-14 10:40:10 +0100

    debug

--- example4.cu
+++ example4.cu
@@ -91,6 +91,7 @@ void example4_ft_antipode() {
     int32_t dummy;
     for (int32_t i=0; i<data.tensor_size; ++i) {
         auto rev_idx = reverse_idx_to(i, data.width, &dummy);
+        std::cout << "idx: " << i << " ridx: " << rev_idx << '\n';
         if ((dummy & 1) == 0) {
             expected.push_back(data.lhs_data[rev_idx]);
         } else {
